3cf13931340c78ccd34a569c4654dc908725e831
smokysk <samuelviana2626@gmail.com>
2020-01-05 14:30:31 -0900

    adcionando a conexaçao ao php

---
+++ conf.php
@@ -0,0 +1,13 @@
+<?php
+    $dsn = "mysql:dbname=login;host=127.0.0.1";
+    $dbuser = "admin";
+    $dbpass = "dascandangas";
+
+    try {
+        $pdo = new PDO($dsn, $dbuser, $dbpass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        //code...
+    } catch (PDOexception $e) {
+        echo "Error: ".$e->getMessage();
+    }
+?>
